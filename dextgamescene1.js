6b62d57cc9a1b6a32e1a00b46fa7dc201d264324
Zahara Spilka <zspilka@macalester.edu>
2020-03-08 19:56:13 -0500

    Update dextgamescene1.js

---
+++ dextgamescene1.js
@@ -0,0 +1,16 @@
+class dextgamescene1 extends Phaser.Scene{
+  constructor(){
+    super({key:"dextgamescene1"});
+  }
+
+  preload(){
+
+  }
+
+  create(){
+    this.createScreenTouch();
+  }
+  
+
+
+}
