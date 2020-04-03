55e5e81f527b86fc9ad686d94782058ae79e30e1
Zahara Spilka <zspilka@macalester.edu>
2020-04-03 10:46:37 -0500

    Got mutlitouch to work so only two fingers are registered

--- dextgamescene1.js
+++ dextgamescene1.js
@@ -3,39 +3,69 @@ class dextgamescene1 extends Phaser.Scene{
     super({key:"dextgamescene1"});
   }
 
+
+
+  // init() {
+  //   //prepare data
+  // }
+
   preload(){
-
-  }
-
-  init(){
-    this.CONFIG = this.sys.game.CONFIG;
-
-    this.generator = new Generator(this);
-
-    //Controls
-    this.is_holding =
+    //USed for load music and pictures
 
   }
 
   create(){
-    this.createScreenTouch();
+    //create objects
+    graphics = this.add.graphics();
+    this.input.addPointer(1);
+    text = this.add.text(10,10, 'Hello');
+    // this.createScreenTouch();
   }
 
-  function createScreenTouch(){
-    this.input.addPointer(2);
-    //
-    // var x = 100;
-    // var y = 0;
-    // var pointer = input.activePointer;
-    // if (pointer.isDown){
-    //   var touchX = pointer.x;
-    //   var touchY = pointer.y;
+  update(){
+    //is a loop that runs constantly
+    if (this.input.pointer1.isDown || this.input.pointer2.isDown){
+      graphics.clear();
+      // text = this.add.text(10,10,"Loading Game...");
+
+      // text = this.add.text(20,20, "Hi", {font: '16px Courier', fill: '#00ff00'});
     }
+    text.setText([
+            'pointer1.isDown: ' + this.input.pointer1.isDown,
+            'pointer2.isDown: ' + this.input.pointer2.isDown,
+            // 'pointer3.isDown: ' + this.input.pointer3.isDown,
+            // 'pointer4.isDown: ' + this.input.pointer4.isDown
+        ]);
 
+        graphics.fillStyle(0xff0000, 1);
+        graphics.fillRect(this.input.pointer1.x, this.input.pointer1.y, 64, 64);
 
+        graphics.fillStyle(0x00ff00, 1);
+        graphics.fillRect(this.input.pointer2.x, this.input.pointer2.y, 64, 64);
 
-    // var hitArea = new Phaser.Geom.Rectangle()
+        // graphics.fillStyle(0x0000ff, 1);
+        // graphics.fillRect(this.input.pointer3.x, this.input.pointer3.y, 64, 64);
+        //
+        // graphics.fillStyle(0xff00ff, 1);
+        // graphics.fillRect(this.input.pointer4.x, this.input.pointer4.y, 64, 64);
+
   }
+
+  // function createScreenTouch(){
+  //   this.input.addPointer(1);
+  //   //
+  //   // var x = 100;
+  //   // var y = 0;
+  //   // var pointer = input.activePointer;
+  //   // if (pointer.isDown){
+  //   //   var touchX = pointer.x;
+  //   //   var touchY = pointer.y;
+  //   // }
+  //
+  //
+  //
+  //   // var hitArea = new Phaser.Geom.Rectangle()
+  // }
 
 
 
--- dextgamesmultitouchscreensetup.js
+++ dextgamesmultitouchscreensetup.js
@@ -2,43 +2,11 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  backgroundColor: 0x000000,
   scene: [dextgamescene1]
-//   scene:{
-//     preload: preload,
-//     create: create,
-//     extend: {
-//       createScreenTouch: createScreenTouch,
-//     }
-//     input: {
-//       avtivePointers: 2,
-//     }
-// }
 };
 
+var text;
+var graphics;
+
 var game = new Phaser.Game(config);
-
-
-// function preload(){
-//
-//
-// }
-//
-// function create(){
-//   this.createScreenTouch();
-// }
-//
-// function createScreenTouch(){
-//   // this.input.addPointer(2);
-//   //
-//   // var x = 100;
-//   // var y = 0;
-//   var pointer = input.activePointer;
-//   if (pointer.isDown){
-//     var touchX = pointer.x;
-//     var touchY = pointer.y;
-//   }
-
-
-
-  // var hitArea = new Phaser.Geom.Rectangle()
-// }
