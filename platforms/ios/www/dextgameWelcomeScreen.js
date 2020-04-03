c0a43c4e62465abb66f0d7ddffa9503e5d743162
Zahara Spilka <zspilka@macalester.edu>
2020-04-03 15:05:50 -0500

    Have Moblie testing working

--- platforms/ios/www/dextgameWelcomeScreen.js
+++ platforms/ios/www/dextgameWelcomeScreen.js
@@ -23,8 +23,24 @@ class dextgameWelcomeScreen extends Phaser.Scene{
   update(){
     //is a loop that runs constantly
     if (this.input.pointer1.isDown || this.input.pointer2.isDown){
-      // graphics.clear();
+      graphics.clear();
       // this.scene.start("playScreen");
     }
+
+    text.setText([
+            'pointer1.isDown: ' + this.input.pointer1.isDown,
+            'pointer2.isDown: ' + this.input.pointer2.isDown,
+
+        ]);
+
+        graphics.fillStyle(0xff0000, 1);
+        graphics.fillRect(this.input.pointer1.x, this.input.pointer1.y, 64, 64);
+
+        graphics.fillStyle(0x00ff00, 1);
+        graphics.fillRect(this.input.pointer2.x, this.input.pointer2.y, 64, 64);
+
+
+
+
   }
 }
