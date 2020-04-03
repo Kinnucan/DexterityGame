83ee141549930cf4dd3877ed5778e8692bf46c45
Zahara Spilka <zspilka@macalester.edu>
2020-04-03 11:08:09 -0500

    Created Welcome, Pause, and Play screens
    Changed names and created new scenes

--- dextgamesmultitouchscreensetup.js
+++ dextgamesmultitouchscreensetup.js
@@ -3,7 +3,7 @@ var config = {
   width: 800,
   height: 600,
   backgroundColor: 0x000000,
-  scene: [dextgameWelcomeScreen]
+  scene: [dextgameWelcomeScreen, dextgamePlayScreen, dextgamePauseScreen]
 };
 
 var text;
