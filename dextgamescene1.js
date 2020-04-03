d26b157f2a470de7ba8d59833387ff808567c496
Zahara Spilka <zspilka@macalester.edu>
2020-04-02 22:52:11 -0500

    Shinnanignas (starting the night codign then things get wack)
    Working on getting mutlitouch enabled and multitaking (saw some really good videos then youtube autoplay took me down a rabbit hole)

--- dextgamescene1.js
+++ dextgamescene1.js
@@ -7,10 +7,36 @@ class dextgamescene1 extends Phaser.Scene{
 
   }
 
+  init(){
+    this.CONFIG = this.sys.game.CONFIG;
+
+    this.generator = new Generator(this);
+
+    //Controls
+    this.is_holding =
+
+  }
+
   create(){
     this.createScreenTouch();
   }
-  
+
+  function createScreenTouch(){
+    this.input.addPointer(2);
+    //
+    // var x = 100;
+    // var y = 0;
+    // var pointer = input.activePointer;
+    // if (pointer.isDown){
+    //   var touchX = pointer.x;
+    //   var touchY = pointer.y;
+    }
+
+
+
+    // var hitArea = new Phaser.Geom.Rectangle()
+  }
+
 
 
 }
--- dextgamesmultitouchscreensetup.js
+++ dextgamesmultitouchscreensetup.js
@@ -3,6 +3,42 @@ var config = {
   width: 800,
   height: 600,
   scene: [dextgamescene1]
+//   scene:{
+//     preload: preload,
+//     create: create,
+//     extend: {
+//       createScreenTouch: createScreenTouch,
+//     }
+//     input: {
+//       avtivePointers: 2,
+//     }
+// }
 };
 
 var game = new Phaser.Game(config);
+
+
+// function preload(){
+//
+//
+// }
+//
+// function create(){
+//   this.createScreenTouch();
+// }
+//
+// function createScreenTouch(){
+//   // this.input.addPointer(2);
+//   //
+//   // var x = 100;
+//   // var y = 0;
+//   var pointer = input.activePointer;
+//   if (pointer.isDown){
+//     var touchX = pointer.x;
+//     var touchY = pointer.y;
+//   }
+
+
+
+  // var hitArea = new Phaser.Geom.Rectangle()
+// }
