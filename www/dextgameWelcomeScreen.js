d55d950a4ea80fbc4a1e6291ab4ce103773392df
Zahara Spilka <zspilka@macalester.edu>
2020-04-16 15:28:27 -0500

    Update dextgameWelcome Screen

--- www/dextgameWelcomeScreen.js
+++ www/dextgameWelcomeScreen.js
@@ -14,15 +14,17 @@ class dextgameWelcomeScreen extends Phaser.Scene{
     //create objects
     graphics = this.add.graphics();
     this.input.addPointer(2);
-    text = this.add.text(20,20, 'Welcome! Place Two Fingers Down To Begin');
+    text = this.add.text(20,20, 'Welcome! Place Two Fingers Down To Begin');    
     this.input.on('pointerdown', function (pointer1) {touchCounter++;}, this);
-    this.input.on('pointerdown', function (pointer2) {touchCounter++;}, this);
+    // this.input.on('pointerdown', function (pointer2) {touchCounter++;}, this);
     // this.scene.start("playScreen");
     // this.input.on('pointerdown', function (pointer1) {touchCounter++;}, this);
     // this.input.on('pointerdown', function (pointer2) {touchCounter++;}, this);
     // if (this.input.on('pointerdown', function(pointer1){}, this) || this.input.on('pointerdown', function(pointer2){}, this)){
     //   this.scene.start('pauseScreen');
-    // }
+    // }// var pointer1 = this.input.activePointer;
+    // var pointer2 = this.input.activePointer;
+
   }
 
   update(){
