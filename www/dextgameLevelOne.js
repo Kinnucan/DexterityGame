8cb9a58e3e055995ba4ea2fdd599a13b24be929c
Paul Cantrell <cantrell@pobox.com>
2020-04-27 14:57:50 -0500

    Moved all tracing responsibility to Tracer class

--- www/dextgameLevelOne.js
+++ www/dextgameLevelOne.js
@@ -7,7 +7,6 @@ class dextgameLevelOne extends Phaser.Scene{
     //Used for load music and pictures
     this.load.image('brush', 'www/img/brush1.png');
     touchCounter = 2;
-    began1 = false;
   }
 
   create(){
@@ -26,76 +25,36 @@ class dextgameLevelOne extends Phaser.Scene{
     var squarePoints = [105, 250, 205, 250, 205, 350, 105, 350];
     var trianglePoints = [407.5, 350, 465, 250, 522.5, 350];
 
-    var tracer1 = new tracer(squarePoints);
-    var tracer2 = new tracer(trianglePoints);
+    var tracer1 = new Tracer(squarePoints);
+    var tracer2 = new Tracer(trianglePoints);
+
+    for (var tracer of [tracer1, tracer2]) {
+      tracer.onPointReached = (x, y) => {
+        this.add.image(x, y, 'brush');
+      };
+    }
 
     //Finds the point that the player is starting nearest on the shape
-    this.input.on('pointerdown', function (pointer1){
+    this.input.on('pointerdown', (pointer1) => {
       var x = pointer1.x;
       var y = pointer1.y;
-      tracer1.findStartingPoint(x, y, tracer1.array);
-      text.setText([tracer1.startingPoint, tracer1.indexOfNextPoint]);
+
+      tracer1.start(x, y);
     }, this);
 
     //Allows user to draw when pressing finger down
     this.input.on('pointermove', function (pointer1) {
-
       //This can be used as an anti-cheating method
       // this.input.on('pointerup', function (pointer1){
       //   this.scene.start("pauseScreen");
       // }, this);
       if (pointer1.isDown){
-        if (!tracer1.checkedDirection){
-          var x = pointer1.x;
-          var y = pointer1.y;
-          tracer1.findDirectionAndNextPoint(x, y, tracer1.array);
-          tracer1.checkedDirectionCounter ++;
-          if (tracer1.checkedDirectionCounter == 30)
-            tracer1.checkedDirection = true;
-        }
-
-        this.add.image(pointer1.x, pointer1.y, 'brush');
-
         var x = pointer1.x;
         var y = pointer1.y;
-        tracer1.traceUserTouch(x, y, tracer1.array);
-      }
-    }, this);
 
+        tracer1.trace(x, y);
 
-    // Finds the point that the player is starting nearest on the shape
-    this.input.on('pointerdown', function (pointer2){
-      var x = pointer2.x;
-      var y = pointer2.y;
-      tracer2.findStartingPoint(x, y, tracer2.array);
-      text2.setText([tracer2.startingPoint, tracer2.indexOfNextPoint]);
-    }, this);
-
-    // Allows user to draw when pressing finger down
-    this.input.on('pointermove', function (pointer2) {
-
-      //This can be used as an anti-cheating method
-      // this.input.on('pointerup', function (pointer1){
-      //   this.scene.start("pauseScreen");
-      // }, this);
-      if (pointer2.isDown){
-        //Timer code
-        timeText.setText('Time: ' + timer + '\nDelta: ');
-
-        if (!tracer2.checkedDirection){
-          var x = pointer2.x;
-          var y = pointer2.y;
-          tracer2.findDirectionAndNextPoint(x, y, tracer2.array);
-          tracer2.checkedDirectionCounter++;
-          if (tracer2.checkedDirectionCounter == 30)
-            tracer2.checkedDirection = true;
-        }
-
-        this.add.image(pointer2.x, pointer2.y, 'brush');
-
-        var x = pointer2.x;
-        var y = pointer2.y;
-        tracer2.traceUserTouch(x, y, tracer2.array);
+        this.add.image(x, y, 'brush').setScale(0.3);
       }
     }, this);
   }
