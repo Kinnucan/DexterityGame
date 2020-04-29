52a3b5b1d63ca605e9c60259691d724da714dc1d
Zahara Spilka <zspilka@macalester.edu>
2020-04-29 00:35:58 -0500

    Tracing algorithm works with new shapes

--- www/dextgamesmultitouchscreensetup.js
+++ www/dextgamesmultitouchscreensetup.js
@@ -43,10 +43,10 @@ function resize() {
 
     const squareRight = {
       name: "square",
-      shapePoints: [105, 250, 226.25, 250, 226.25, 371.25, 105, 371.25],
+      shapePoints: [505, 250, 626.25, 250, 626.25, 371.25, 505, 371.25],
       draw: function(){
         graphics.lineStyle(5, 0xFF000, 1.0);
-        graphics.strokeRect(105, 250, 121.25, 121.25);
+        graphics.strokeRect(505, 250, 121.25, 121.25);
       }
     };
     const triangleRight = {
@@ -59,6 +59,7 @@ function resize() {
     };
 
 const rightShapeList = [squareRight, triangleRight];
+// const rightShapeList = [squareRight];
 
 
 
@@ -72,14 +73,15 @@ const squareLeft = {
 };
 const triangleLeft = {
   name: "triangle",
-  shapePoints: [400, 371.25, 470, 250, 540, 371.25],
+  shapePoints: [100, 371.25, 170, 250, 240, 371.25],
   draw: function() {
     graphics.lineStyle(5, 0x0000FF, 1.0);
-    graphics.strokeTriangle(400, 371.25, 470, 250, 540, 371.25);
+    graphics.strokeTriangle(100, 371.25, 170, 250, 240, 371.25);
   }
 };
 
 const leftShapeList = [squareLeft, triangleLeft];
+// const leftShapeList = [triangleLeft];
 
 
 
