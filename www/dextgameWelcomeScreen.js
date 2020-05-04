5da3c958d1e477d5127f98cfcbc8bdffb33df930
Kinnucan <kinnucanr@gmail.com>
2020-05-04 11:36:52 -0500

    just removed some unused code

--- www/dextgameWelcomeScreen.js
+++ www/dextgameWelcomeScreen.js
@@ -16,43 +16,6 @@ class dextgameWelcomeScreen extends Phaser.Scene{
 
     touchCounter = 0;
     //create objects
-
-    //For circle/square
-    // graphics = this.add.graphics({ fillStyle: { color: 0x9400D3 } });
-
-    //For line, color = purple
-    // graphics = this.add.graphics({ lineStyle: { width: 4, color: 0x9400D3 } });
-    //For line, color = neon green
-    // graphics = this.add.graphics({ lineStyle: { width: 6, color: 0xFF000 } });
-    //
-    // var started = false;
-    // var prevX;
-    // var prevY;
-    //
-    // this.input.on('pointermove', (pointer) => {
-    //     var x = pointer.x;
-    //     var y = pointer.y;
-    //     // if (started){
-    //     //   var line = new Phaser.Geom.Line(prevX, prevY, x, y);
-    //     //   graphics.strokeLineShape(line);
-    //     // }
-    //     // if (!started){
-    //     //   started = true;
-    //     // }
-    //     // prevX = x;
-    //     // prevY = y;
-    //
-    //     // var circle = new Phaser.Geom.Rectangle(x, y, 10, 10);
-    //     // graphics.fillRectShape(circle);
-    // }, this);
-    // graphics = this.add.graphics({ fillStyle: { color: 0x9400D3 } });
-    //
-    // this.input.on('pointermove', (pointer) => {
-    //     var x = pointer.x;
-    //     var y = pointer.y;
-    //     var circle = new Phaser.Geom.Circle(x, y, 8);
-    //     graphics.fillCircleShape(circle);
-    // }, this);
     // const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
     // const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
     this.input.addPointer(1);
