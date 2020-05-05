b1ae2b99122ec29e773dc7ddeb9eb719e7386dc9
Zahara Spilka <zspilka@macalester.edu>
2020-05-04 19:05:01 -0500

    Update dextgameWelcomeScreen.js

--- www/dextgameWelcomeScreen.js
+++ www/dextgameWelcomeScreen.js
@@ -16,7 +16,7 @@ class dextgameWelcomeScreen extends Phaser.Scene{
 
     ship = this.add.image(0, 0, 'pic').setOrigin(0);
     orientationText = this.add.text(320, 128, 'Please set your\nphone to landscape', { font: '48px Courier', fill: '#00ff00', align: 'center' }).setOrigin(0.5);
-    checkOriention(this.scale.orientation);
+    checkOriention(screen.orientation);
     this.scale.on('orientationchange', checkOriention, this);
 
 
