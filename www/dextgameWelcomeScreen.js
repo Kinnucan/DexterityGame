23bf7fbb947ca153b7e56643db0cab0056d05f97
Zahara Spilka <zspilka@macalester.edu>
2020-05-06 13:13:28 -0500

    Fixed the WinScreens appearence

--- www/dextgameWelcomeScreen.js
+++ www/dextgameWelcomeScreen.js
@@ -26,8 +26,7 @@ class dextgameWelcomeScreen extends Phaser.Scene{
     //is a loop that runs constantly
     checkOriention(window.innerWidth, window.innerHeight);
     if (touchCounter == 2){
-      this.scene.start("winScreen");
-      // this.scene.start("rulesScreen");
+      this.scene.start("rulesScreen");
     }
   }
 
