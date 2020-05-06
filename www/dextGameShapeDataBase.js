a2ef56b1815f6c314a225e07fad71d598c6ff707
Zahara Spilka <zspilka@macalester.edu>
2020-05-06 18:00:23 -0500

    Update dextGameShapeDataBase.js

--- www/dextGameShapeDataBase.js
+++ www/dextGameShapeDataBase.js
@@ -1,6 +1,36 @@
+var arrow = {
+  name: "arrow",
+  shapePoints: [[-75,46.25],[-75,16.25],[0,16.25],[0,-6.25],[75,46.25],[0,98.75],[0,76.25],[-75,76.25]],
+  hasUsed: false
+};
+
 var diamond = {
   name: "diamond",
   shapePoints: [[-60.625, 60.625], [0, 0], [60.625, 60.625], [0, 121.25]],
+  hasUsed: false
+};
+
+var heptagon = {
+  name: "heptagon",
+  shapePoints: [[-55.625, 25.625], [0, -5], [55.625, 25.625], [70.625, 80.9375], [35.625, 121.25], [0, 121.25], [-35.625, 121.25], [-70.625, 80.9375]],
+  hasUsed: false
+};
+
+var hexagon = {
+  name: "hexagon",
+  shapePoints: [[-75, 46.25], [-37.5,-8.75], [0, -8.75], [37.5, -8.75], [75, 46.25], [37.5, 101.25], [0, 101.25], [-37.5,101.25]],
+  hasUsed: false
+};
+
+var octagon = {
+  name: "octagon",
+  shapePoints: [[-75, 16.25], [-30,-28.75], [0, -28.75], [30, -28.75],[75, 16.25], [75, 76.25], [30, 121.25], [0, 121.25], [-30, 121.25], [-75, 76.25]],
+  hasUsed: false
+};
+
+var pentagon = {
+  name: "pentagon",
+  shapePoints: [[-60.625, 50.625], [0, -5], [60.625, 50.625], [40.625, 121.25], [0, 121.25], [-40.625, 121.25]],
   hasUsed: false
 };
 
@@ -28,35 +58,25 @@ var star = {
   hasUsed: false
 };
 
-var triangle = {
-  name: "triangle",
-  shapePoints: [[-70, 121.25], [0, 0], [70, 121.25]],
-  hasUsed: false
-};
-
 var trapezoid = {
   name: "trapezoid",
   shapePoints: [[-60.625, 0], [60.625, 0], [121.25, 121.25], [0, 121.25], [-121.25, 121.25]],
   hasUsed: false
 };
 
-var pentagon = {
-  name: "pentagon",
-  shapePoints: [[-60.625, 50.625], [0, -5], [60.625, 50.625], [40.625, 121.25], [0, 121.25], [-40.625, 121.25]],
+var triangle = {
+  name: "triangle",
+  shapePoints: [[-70, 121.25], [0, 0], [70, 121.25]],
   hasUsed: false
 };
 
-var hexagon = {
-  name: "hexagon",
-  shapePoints: [[-45.625, 0], [0, 0], [45.625, 0], [75.625, 60.625], [45.625, 121.25], [0, 121.25], [-45.625, 121.25], [-75.625, 60.625]],
-  hasUsed: false
-};
 
-var heptagon = {
-  name: "heptagon",
-  shapePoints: [[-55.625, 25.625], [0, -5], [55.625, 25.625], [70.625, 80.9375], [35.625, 121.25], [0, 121.25], [-35.625, 121.25], [-70.625, 80.9375]],
-  hasUsed: false
-};
+
+
+
+
+
+
 
 // var pentagon = {
 //   name: "pentagon",
@@ -64,4 +84,5 @@ var heptagon = {
 //   hasUsed: false
 // };
 
-var shapeList = [diamond, rectangle, rhombus, square, star, triangle, trapezoid, pentagon, hexagon, heptagon];
+// var shapeList = [arrow, diamond, heptagon, hexagon, octagon, pentagon, rectangle, rhombus, square, star, triangle, trapezoid];
+var shapeList = [arrow,hexagon];
